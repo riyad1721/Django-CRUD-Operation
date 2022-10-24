fd8f617b5ff1b09b7ec4ee30908829508c5ead84
Your Name <you@example.com>
2022-10-24 12:26:21 +0600

    get or read  operation

---
+++ api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import StudentSerilizer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+# Create your views here.
+
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id',None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerilizer(stu)
+            json_data = JSONRenderer().render(serializer.data)#
+            return HttpResponse(json_data, content_type = 'application/api')
+        stu = Student.objects.all()
+        serializer = StudentSerilizer(stu,many=True)
+        json_data = JSONRenderer().render(serializer.data)#
+        return HttpResponse(json_data, content_type = 'application/api')
